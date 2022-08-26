e07cd132adaa540d959980c0f397a86b907ff040
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-08-26 20:54:58 +0530

    fix: Table styling

--- src/components/Outputtable.jsx
+++ src/components/Outputtable.jsx
@@ -1,21 +1,42 @@
 import getBalance from "../utils/getBalance";
+// import { colors } from "../theme/root";
+import { transparentColorBox } from "../theme/styles";
+import { Box, Container, TableContainer, TableBody, TableHead, TableCell, TableRow, Table, Paper } from "@mui/material";
 
 function Outputtable( { visibility, bill, amount, denomination } ) {
     
-    const NEW_CURRENCY = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-    const OLD_CURRENCY = [1000, 500, 100, 50, 20, 10, 5, 2, 1];
-    const CURRENT_CURRENCY = denomination === "new" ? NEW_CURRENCY : OLD_CURRENCY
+    function getDenominationArray(denomination) {
+        const NEW_CURRENCY = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+        const OLD_CURRENCY = [1000, 500, 100, 50, 20, 10, 5, 2, 1];
+        return (denomination === "new" ? NEW_CURRENCY : OLD_CURRENCY);
+    }
+
+    const CURRENT_CURRENCY = getDenominationArray(denomination)
     const balanceObject = getBalance(amount, bill, CURRENT_CURRENCY);
 
     return ( 
         <>
-            <div style={{visibility: visibility? "unset" : "hidden"}}>
-                <table>
-                    <thead>Balance Currency Breakout</thead>
-                    <tr><th>Number</th> {Object.values(balanceObject).map((element) => <td>{element}</td>)} </tr>
-                    <tr><th>Notes</th> {Object.keys(balanceObject).map((element) => <td>{element}</td>)} </tr>
-                </table>
-            </div>
+            <Box sx={{...transparentColorBox, visibility: visibility? "unset" : "hidden"}}>
+                <Container maxWidth="md" >
+                    <h2>Balance Currency Breakout</h2>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Denomination</TableCell>
+                                    {Object.keys(balanceObject).map((element) => <TableCell align="center" key={element} >{element}</TableCell>)}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Number of Notes</TableCell>
+                                    {Object.values(balanceObject).map((element, index) => <TableCell align="center" key={Object.keys(balanceObject)[index]} >{element}</TableCell>)}
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Container>
+            </Box>
         </>
      );
 }
